7ad9fb465eaab3f913e3f620d7275f2d629c7c71
jinhyeyeon <jhy199@naver.com>
2019-01-12 20:32:08 +0900

    Modal textarea에 value라는 state값 추가
    #issue-4

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,8 +4,13 @@ import Btn1 from '../Btn/Btn1/Btn1';
 import {IoMdClose, IoMdCreate} from "react-icons/io";
 
 class Modal extends React.Component {
+    state = {
+        value: ''
+    };
+
     render() {
-        const {onClose} =this.props;
+        const {onClose} = this.props;
+        const {value} = this.state;
         return (
             <div className="modal">
                 <div className="dim-bg"
@@ -14,6 +19,8 @@ class Modal extends React.Component {
                 <div className="write-box">
                     <h2>새 글 작성하기</h2>
                     <textarea
+                        value={value}
+                        onChange={(e) => this.setState({value: e.target.value})}
                         placeholder="새 글을 작성해주세요."
                     />
                     <Btn1>
