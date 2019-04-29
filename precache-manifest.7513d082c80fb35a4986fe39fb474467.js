65964afefc4e607cead6acff9511bc3cdf98bb0b
jinhyeyeon <hy.jin@balky.kr>
2019-04-29 13:19:47 +0900

    Updates

---
+++ precache-manifest.7513d082c80fb35a4986fe39fb474467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85bc39587fca2363c7ec",
+    "url": "/basicTodoList/static/css/main.e059d4bb.chunk.css"
+  },
+  {
+    "revision": "85bc39587fca2363c7ec",
+    "url": "/basicTodoList/static/js/main.85bc3958.chunk.js"
+  },
+  {
+    "revision": "623b2104bebe4c743613",
+    "url": "/basicTodoList/static/js/1.623b2104.chunk.js"
+  },
+  {
+    "revision": "ee47fc2f3ec27f527819",
+    "url": "/basicTodoList/static/js/runtime~main.ee47fc2f.js"
+  },
+  {
+    "revision": "7285488336577ce456e332bc6051c4fd",
+    "url": "/basicTodoList/index.html"
+  }
+];
